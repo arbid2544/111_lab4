37b178b046861e8c490fcf85e649a76e2dbe6931
arbid2544 <mrarbid.m@ku.th>
2021-03-01 19:56:21 +0700

    Update 111_lab4-1.cpp

--- 111_lab4-1.cpp
+++ 111_lab4-1.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	int n,i,j;
+	int n,i,j,max=0,max2,min=2000,min2=0,sum=0;
     scanf("%d",&n);
     int a[n][3];
     for(i=0;i<n;i++)
@@ -11,13 +11,27 @@ int main()
             scanf("%d",&a[i][j]);
         }
     }
-    int sum=0;
     for(i=0;i<n-2;i++)
     {
         for(j=i;j<i+3;j++)
         {
             sum+=(a[j][0]*4)+(a[j][1]*2)+(a[j][2]*1);
+            //printf("%d|",sum);
         }
+        if(max<sum)
+        {
+            max=sum;
+            max2=i;
+          //  printf("%d|",max2);
+        }
+        if(min>sum)
+        {
+            min=sum;
+            min2=i;
+            //printf("%d|",min2);
+        }
+        sum=0;
     }
+    printf("%d %d",max2+1,min2+1);
 	return 0;
 }
