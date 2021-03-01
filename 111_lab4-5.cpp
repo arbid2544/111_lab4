bdb8f099a864ec03ee36f2ff793f8b7a5092b083
arbid2544 <mrarbid.m@ku.th>
2021-03-01 18:36:17 +0700

    Update 111_lab4-5.cpp

--- 111_lab4-5.cpp
+++ 111_lab4-5.cpp
@@ -4,45 +4,9 @@ int main()
 	char letters[50];
     int glass[4]={1,0,0,0},n,w,x,i,max=0;
     scanf("%s",letters);
+    
 	while(letters[n]!='\0')
 	{
-		/*switch (letters[n])
-		{
-		case 'A':
-		    w=glass[2];
-		    glass[2]=glass[1];
-		    glass[1]=w;
-		    break;
-		case 'B':
-		    w=glass[3];
-		    glass[3]=glass[0];
-		    glass[0]=w;
-		    break;
-		case 'C':
-		    w=glass[0];
-		    glass[0]=glass[2];
-		    glass[2]=w;
-		    x=glass[1];
-		    glass[1]=glass[3];
-		    glass[3]=x;
-		    break;
-		case 'D':
-			w=glass[0];
-		    glass[0]=glass[1];
-		    glass[1]=w;
-		    x=glass[2];
-		    glass[2]=glass[3];
-		    glass[3]=x;
-		    break;
-		case 'E':
-			w=glass[2];
-		    glass[2]=glass[1];
-		    glass[1]=w;
-		    x=glass[3];
-		    glass[3]=glass[0];
-		    glass[0]=x;
-		    break;
-	    }*/
 	    if(letters[n]==65)
 	    {
 	    	w=glass[2];
@@ -88,16 +52,15 @@ int main()
 	{
 		if(max<glass[i])
          max=i;
-        printf("%d",glass[i]);
 	}
        
     if(max==0)
-       printf("\n1");
+       printf("1");
     if(max==1)
-       printf("\n2");
+       printf("2");
     if(max==2)
-       printf("\n3");
+       printf("3");
     if(max==3)
-       printf("\n4");
+       printf("4");
 	return 0;
 }
