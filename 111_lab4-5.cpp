3b8cb1f3d8bd5f43c106f63294da0b80ab925038
arbid2544 <mrarbid.m@ku.th>
2021-03-01 19:36:09 +0700

    Update 111_lab4-5.cpp

--- 111_lab4-5.cpp
+++ 111_lab4-5.cpp
@@ -2,9 +2,11 @@
 int main()
 {
 	char letters[50];
-    int glass[4]={1,0,0,0},n,w,x,i,max=0;
+    int glass[4]={1,0,0,0},n=0,w,x,i,z,m;
+    scanf("%d",&m);
+    int max[m]={0};
+    for(z=0;z<m;z++){
     scanf("%s",letters);
-    
 	while(letters[n]!='\0')
 	{
 	    if(letters[n]==65)
@@ -12,12 +14,14 @@ int main()
 	    	w=glass[2];
 		    glass[2]=glass[1];
 		    glass[1]=w;
+		  //  printf("%d %d %d %d|",glass[0],glass[1],glass[2],glass[3]);
 		}
 	    if(letters[n]==66)
 	    {
 	    	w=glass[3];
 		    glass[3]=glass[0];
 		    glass[0]=w;
+		  //  printf("%d %d %d %d|",glass[0],glass[1],glass[2],glass[3]);
 		}
 		if(letters[n]==67)
 		{
@@ -27,6 +31,7 @@ int main()
 		    x=glass[1];
 		    glass[1]=glass[3];
 		    glass[3]=x;
+		   // printf("%d %d %d %d|",glass[0],glass[1],glass[2],glass[3]);
 		}
 		if(letters[n]==68)
 		{
@@ -36,6 +41,7 @@ int main()
 		    x=glass[2];
 		    glass[2]=glass[3];
 		    glass[3]=x;
+		    //printf("%d %d %d %d|",glass[0],glass[1],glass[2],glass[3]);
 		}
 		if(letters[n]==69)
 		{
@@ -45,22 +51,31 @@ int main()
 		    x=glass[3];
 		    glass[3]=glass[0];
 		    glass[0]=x;
+		    //printf("%d %d %d %d|",glass[0],glass[1],glass[2],glass[3]);
 		}
+	//	printf("%d",glass[n]);
 		n++;
 	}
+	n=0;
 	for(i=0;i<4;i++)
 	{
-		if(max<glass[i])
-         max=i;
+		if(max[z]<glass[i])
+         max[z]=i;
+        //printf("%d",max[z]);
 	}
-       
-    if(max==0)
-       printf("1");
-    if(max==1)
-       printf("2");
-    if(max==2)
-       printf("3");
-    if(max==3)
-       printf("4");
+}
+
+    for(z=0;z<m;z++)
+	{
+    if(max[z]==0)
+       printf("1\n");
+    if(max[z]==1)
+       printf("2\n");
+    if(max[z]==2)
+       printf("3\n");
+    if(max[z]==3)
+       printf("4\n");
+   }
+   //printf("%d %d %d %d",glass[0],glass[1],glass[2],glass[3]);
 	return 0;
 }
