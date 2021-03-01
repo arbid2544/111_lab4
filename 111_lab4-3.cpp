21aaa316ed7dbcae560085a6dd0d4a54774d82f4
arbid2544 <mrarbid.m@ku.th>
2021-03-01 17:10:00 +0700

    Update 111_lab4-3.cpp

--- 111_lab4-3.cpp
+++ 111_lab4-3.cpp
@@ -1,7 +1,15 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
 	char letters[200];
-	int n;
+	int n=0;
+	fgets(letters,sizeof letters,stdin);
+	while(letters[n]!='\0')
+	{
+		if(letters[n]!=letters[n+1])
+		printf("%c",letters[n]);
+		n++;
+	}
 	return 0;
 }
