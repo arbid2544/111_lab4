d6c2df24641add918b91a7f4300f8f8c08bb27a7
arbid2544 <mrarbid.m@ku.th>
2021-03-01 19:48:32 +0700

    Update 111_lab4-1.cpp

--- 111_lab4-1.cpp
+++ 111_lab4-1.cpp
@@ -1,14 +1,22 @@
 #include<stdio.h>
 int main()
 {
-	int n;
+	int n,i,j;
     scanf("%d",&n);
     int a[n][3];
-    for(int i=0;i<n;i++)
+    for(i=0;i<n;i++)
     {
-        for(int j=0;j<3;j++)
+        for(j=0;j<3;j++)
         {
             scanf("%d",&a[i][j]);
+        }
+    }
+    int sum=0;
+    for(i=0;i<n-2;i++)
+    {
+        for(j=i;j<i+3;j++)
+        {
+            sum+=(a[j][0]*4)+(a[j][1]*2)+(a[j][2]*1);
         }
     }
 	return 0;
