94436f5324fd6fa8b1131a1d5079e50a5e4742bd
arbid2544 <mrarbid.m@ku.th>
2021-03-01 18:14:12 +0700

    Update 111_lab4-5.cpp

--- 111_lab4-5.cpp
+++ 111_lab4-5.cpp
@@ -2,20 +2,70 @@
 int main()
 {
 	char letters[50];
-    int glass[2][2]={1,0,0,0},n;
-    scanf("%d",&n);
-    fflush(stdin);
-    fgets(letters,sizeof letters,stdin);
+    int glass[4]={1,0,0,0},n,w,x,i,max=0;
+    scanf("%s",letters);
 	while(letters[n]!='\0')
 	{
-		switch (letters[n])
+		/*switch (letters[n])
 		{
-		case 'A':;
-		case 'B':;
-		case 'C':;
-		case 'D':;
-	    }
+		case 'A':
+		    w=glass[2];
+		    glass[2]=glass[1];
+		    glass[1]=w;
+		    break;
+		case 'B':
+		    w=glass[3];
+		    glass[3]=glass[0];
+		    glass[0]=w;
+		    break;
+		case 'C':
+		    w=glass[0];
+		    glass[0]=glass[2];
+		    glass[2]=w;
+		    x=glass[1];
+		    glass[1]=glass[3];
+		    glass[3]=x;
+		    break;
+		case 'D':
+			w=glass[0];
+		    glass[0]=glass[1];
+		    glass[1]=w;
+		    x=glass[2];
+		    glass[2]=glass[3];
+		    glass[3]=x;
+		    break;
+		case 'E':
+			w=glass[2];
+		    glass[2]=glass[1];
+		    glass[1]=w;
+		    x=glass[3];
+		    glass[3]=glass[0];
+		    glass[0]=x;
+		    break;
+	    }*/
+	    if(letters[n]==66)
+	    {
+	    	w=glass[3];
+		    glass[3]=glass[0];
+		    glass[0]=w;
+		    printf("Ok");
+		}
 		n++;
 	}
+	for(i=0;i<4;i++)
+	{
+		if(max<glass[i])
+         max=i;
+        printf("%d",glass[i]);
+	}
+       
+    if(max==0)
+       printf("\n1");
+    if(max==1)
+       printf("\n2");
+    if(max==2)
+       printf("\n3");
+    if(max==3)
+       printf("\n4");
 	return 0;
 }
