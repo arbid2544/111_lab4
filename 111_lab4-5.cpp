69bbd1192f0ef4c5b189a85823eebe09be3c76af
arbid2544 <mrarbid.m@ku.th>
2021-03-01 17:34:34 +0700

    Update 111_lab4-5.cpp

--- 111_lab4-5.cpp
+++ 111_lab4-5.cpp
@@ -4,15 +4,16 @@ int main()
 	char letters[50];
     int glass[2][2]={1,0,0,0},n;
     scanf("%d",&n);
+    fflush(stdin);
     fgets(letters,sizeof letters,stdin);
 	while(letters[n]!='\0')
 	{
 		switch (letters[n])
 		{
-		case 'A':printf("A");
-		case 'B':printf("B");
-		case 'C':printf("C");
-		case 'D':printf("D");
+		case 'A':;
+		case 'B':;
+		case 'C':;
+		case 'D':;
 	    }
 		n++;
 	}
